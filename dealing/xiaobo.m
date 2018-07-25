06d089f89d171ad437178792af126f78a43b53a6
密码111111 <23580151@qq.com>
2018-07-25 21:28:30 +0800

    change xiaobo

--- dealing/xiaobo.m
+++ dealing/xiaobo.m
@@ -1,12 +1,16 @@
 function y = xiaobo(jz,dnum,num,znum)
+y=zeros(znum,dnum);
+plot(jz(1:num,:));
 for i=1:1:dnum
     if num>znum 
         num=znum;
     end
     x=1:znum/num:znum;
     x1=1:1:znum;
-    s1=mapminmax(interp1(x,jz(1:num,i),x1,'nearest'), 0, 1);
+    [s1,~]=mapminmax(interp1(x,jz(1:num,i),x1,'spline','extrap'), 0, 1);%Use extrap method to avoid NaN
     y(1:znum,i)=s1;
 end
+hold on;
+plot(y(1:znum,:));
 
 
